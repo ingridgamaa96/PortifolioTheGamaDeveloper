0dde27396df56c57f2beb14a808c2668c7cadb51
Ingridg <49728531+ingridgamaa96@users.noreply.github.com>
2023-06-28 20:12:20 -0300

    incluindo className e responsividade

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,12 +1,31 @@
-
+import Image from "next/image"
 export const HeroSection = () => {
     return (
-        <section className="w-full h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex-col justify-end">
-            <div className="container">
+        <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
+         <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
+                <div className="w-full lg:max-[530px]:">
+                    <p className="font-mono text-emerald-400">Oi , meu nome é</p>
+                    <h2 className="text4xl font-medium mt-2"> Ingrid Gama </h2>
+
+                   <p className="text-gray-400 my-6 text-sm sm:text-base">Oi meu nome é Ingrid gama eu sou Desenvolvedor Beck-And 
+                      </p>
+
                 <div>
-                    <p>Hello, My Name Is Ingrid </p>
+                    techs
                 </div>
 
+                <div>
+                    contato
+                </div>
+            </div>
+
+                <Image
+                className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover" 
+                width={420}
+                height={404}
+                src="/images/profile-pic.png"
+                alt="Foto Perfil Ingrid Gama"
+                />
             </div>
         </section>
     )
