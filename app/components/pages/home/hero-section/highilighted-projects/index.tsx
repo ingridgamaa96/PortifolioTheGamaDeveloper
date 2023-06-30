5638e1cd1acc44ca136112bc3f1acfba88532e6c
Ingridg <49728531+ingridgamaa96@users.noreply.github.com>
2023-06-30 19:27:42 -0300

    incluindo projetos

---
+++ app/components/pages/home/hero-section/highilighted-projects/index.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from "@/app/components/section-tittle"
+
+export const HighlightedProjects = () => {
+    return (
+        <section className="container py-16">
+            <SectionTitle subtitle="destaques" title="Projetos em destaque"/>
+        </section>
+    )
+}
