c971aed26d9a2e0f12ac7a80c8c7f140ba5fd57c
Ingridg <49728531+ingridgamaa96@users.noreply.github.com>
2023-06-30 13:19:58 -0300

    incluindo competencias

---
+++ app/components/pages/known-techs/index.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from "../../section-tittle"
+
+export const KnowTechs = () => {
+    return (
+        <section className="container py-16">
+            <SectionTitle subtitle="competências" title="Conhecimentos"/>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import {HeroSection} from "./components/pages/home/hero-section"
+import { KnowTechs } from "./components/pages/known-techs"
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <KnowTechs />
     </>
   )
 }
