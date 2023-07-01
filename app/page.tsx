1fde6d8df6976eab72200203e6720a96d4188477
Ingridg <49728531+ingridgamaa96@users.noreply.github.com>
2023-07-01 14:36:57 -0300

    components page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import {HeroSection} from "./components/pages/home/hero-section"
+import { HighlightedProjects } from "./components/pages/home/hero-section/highilighted-projects"
 import { KnowTechs } from "./components/pages/known-techs"
 export default async function Home() {
   return (
     <>
       <HeroSection />
       <KnowTechs />
+      <HighlightedProjects />
     </>
   )
 }
